051cabe8c6e5559a49de953f1f80fb7c5ca5f7d2
Tariq Safieh <safieh.tariq@gmail.com>
2024-07-18 07:16:14 +0000

    Add calculator

--- app_pages/page_calculator.py
+++ app_pages/page_calculator.py
@@ -1,2 +1,17 @@
+import streamlit as st
 
+def calculator_body():
+    st.write("---")
+    a = st.number_input("Enter a number", 0)
+    b = st.number_input("Enter another number", 0)
+    operation = st.selectbox("Operation", ["Add", "Subtract", "Multiply", "Divide"])
+    if operation == "Add":
+        result = a + b
+    elif operation == "Subtract":
+        result = a - b
+    elif operation == "Multiply":
+        result = a * b
+    elif operation == "Divide":
+        result = a / b
+    st.write(f"Result: {result}")
     
